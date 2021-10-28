2e2e9a8ad0ee84094388d2583f55ec1cba146225
rahaljaya2012@gmail.com <rahaljaya2012@gmail.com>
2021-10-28 20:01:18 +0530

    setCard method added

--- web/accounts.dart
+++ web/accounts.dart
@@ -15,14 +15,14 @@ Accounts(this.cardName, this.cardNumber, this.cardHolderName, this.expDate, this
  this.detailAmount) {}
 
 String setCard(String cardName, String cardNumber, String cardHolderName, String expDate, String cardColor) {
-  String div = 
-  "<div style='color: var(--bs-pink);background: var(--bs-pink);width: 262.656px;height: 152px;padding: 6px;margin: 13px;box-shadow: 0px 0px 20px var(--bs-body-color);'> " +
-	   "<label class='form-label' style='color: var(--bs-gray-900);text-align: right;font-weight: bold;margin-left: 195px;'>Card 01</label> " +
-     "<h1 style='color: var(--bs-gray-100);font-size: 26px;height: auto;width: auto;margin-top: 0px;margin-right: 0px;text-align: center;box-shadow: 0px 0px;'>4220 1254 1254 4851</h1> " +
-     "<h1 style='color: var(--bs-gray-100);font-size: 22px;height: auto;width: auto;margin-top: 24px;text-align: left;box-shadow: 0px 0px;margin-left: 8px;margin-bottom: 14px;'>Admin User</h1> " +
-      "<h1 style='color: var(--bs-gray-100);font-size: 19px;height: auto;width: auto;margin-top: -14px;margin-right: 0px;text-align: left;box-shadow: 0px 0px;margin-left: 8px;'>01/22     </h1> "+
-  "</div> ";
-  return div;
+  var cardDiv = 
+  "<div style=';background: var("+ cardColor +');width: 262.656px;height: 152px;padding: 6px;margin: 13px;box-shadow: 0px 0px 20px var(--bs-body-color);'> " +
+	   "<label class='form-label' style='color: var(--bs-gray-900);text-align: right;font-weight: bold;margin-left: 195px;'>"+ cardName +'</label> ' +
+     "<h1 style='color: var(--bs-gray-100);font-size: 26px;height: auto;width: auto;margin-top: 0px;margin-right: 0px;text-align: center;box-shadow: 0px 0px;'>"+ cardNumber +'</h1> ' +
+     "<h1 style='color: var(--bs-gray-100);font-size: 22px;height: auto;width: auto;margin-top: 24px;text-align: left;box-shadow: 0px 0px;margin-left: 8px;margin-bottom: 14px;'>"+ cardHolderName +'</h1> ' +
+      "<h1 style='color: var(--bs-gray-100);font-size: 19px;height: auto;width: auto;margin-top: -14px;margin-right: 0px;text-align: left;box-shadow: 0px 0px;margin-left: 8px;'>"+ expDate +'</h1> </div>';
+  
+  return cardDiv;
 }
 
 }
