c6f8231220ac76655e1c1fe6f25b1b71ef1b6a59
rahaljaya2012@gmail.com <rahaljaya2012@gmail.com>
2021-11-02 22:26:46 +0530

    fix dispaly method

--- web/display.dart
+++ web/display.dart
@@ -9,26 +9,21 @@ class Display {
 
 List<String> populateList() {
   var x = new Accounts();
-  cards.add(x.setCard("card 01","2336 5221 5487 2655","Test user","03/01","blue"));
+  cards.add(x.setCard("card 01","2336 5221 5487 2655","Test user","03/01","--bs-pink"));
   return cards;
 }
 
-void createDiv() {
+String createDiv() {
+  populateList();
   DivElement outDiv = new DivElement();
-  //outDiv = cards;
-  Element e = querySelector('table');
-  
-  String someValue = 'hello, world';
-  int anotherValue = 23958;
-  
-  Element row = new Element.tr()
-    ..appendHtml('''
-    	<td>$someValue</td>
-      <td>$anotherValue</td>
-    ''');
-  
-  e.append(row);
-  
+  String divCard = "";
+  for (var card in cards) {
+    //outDiv.innerHtml = card;
+    divCard = card;
+  }
+  //print(outDiv);
+  return divCard;
+
 
 }
 
